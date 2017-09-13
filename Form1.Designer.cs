b300e0b6a8beb3018410572173d8408ea8f1f96c
DrFR0ST <mike.eling97@gmail.com>
2017-09-13 12:41:10 +0200

    Release v1.0.0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CPickX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Update_timer = new System.Windows.Forms.Timer(this.components);
+            this.color_lbl = new MaterialSkin.Controls.MaterialLabel();
+            this.colorPanel_pnl = new System.Windows.Forms.Panel();
+            this.pick_btn = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Update_timer
+            // 
+            this.Update_timer.Interval = 10;
+            this.Update_timer.Tick += new System.EventHandler(this.Update_timer_Tick);
+            // 
+            // color_lbl
+            // 
+            this.color_lbl.AutoSize = true;
+            this.color_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.color_lbl.Depth = 0;
+            this.color_lbl.Font = new System.Drawing.Font("Roboto", 11F);
+            this.color_lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.color_lbl.Location = new System.Drawing.Point(12, 184);
+            this.color_lbl.MouseState = MaterialSkin.MouseState.HOVER;
+            this.color_lbl.Name = "color_lbl";
+            this.color_lbl.Size = new System.Drawing.Size(202, 19);
+            this.color_lbl.TabIndex = 0;
+            this.color_lbl.Text = "Copyright © Mike Eling, 2017";
+            // 
+            // colorPanel_pnl
+            // 
+            this.colorPanel_pnl.Location = new System.Drawing.Point(12, 70);
+            this.colorPanel_pnl.Name = "colorPanel_pnl";
+            this.colorPanel_pnl.Size = new System.Drawing.Size(405, 100);
+            this.colorPanel_pnl.TabIndex = 1;
+            // 
+            // pick_btn
+            // 
+            this.pick_btn.AutoSize = true;
+            this.pick_btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pick_btn.BackColor = System.Drawing.Color.Transparent;
+            this.pick_btn.Depth = 0;
+            this.pick_btn.Icon = null;
+            this.pick_btn.Location = new System.Drawing.Point(333, 176);
+            this.pick_btn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.pick_btn.Name = "pick_btn";
+            this.pick_btn.Primary = true;
+            this.pick_btn.Size = new System.Drawing.Size(51, 36);
+            this.pick_btn.TabIndex = 3;
+            this.pick_btn.Text = "pick";
+            this.pick_btn.UseVisualStyleBackColor = false;
+            this.pick_btn.Click += new System.EventHandler(this.pick_btn_Click);
+            this.pick_btn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::CPickX.Properties.Resources.ic_color_lens_white_48dp;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(396, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 218);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pick_btn);
+            this.Controls.Add(this.colorPanel_pnl);
+            this.Controls.Add(this.color_lbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(431, 218);
+            this.MinimumSize = new System.Drawing.Size(431, 218);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cPick";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Update_timer;
+        private MaterialSkin.Controls.MaterialLabel color_lbl;
+        private System.Windows.Forms.Panel colorPanel_pnl;
+        private MaterialSkin.Controls.MaterialRaisedButton pick_btn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace CPickX
+{
+    public partial class Form1 : MaterialForm
+    {
+        Color c = new Color();
+        Form previewF;
+        Form previewFshadow;
+
+        bool hoverForm = false;
+
+        bool isActive = false;
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Red500, Accent.Indigo200, TextShade.WHITE);
+
+            MouseHook.Start();
+            MouseHook.MouseAction += new EventHandler(MouseLeftClick);
+        }
+
+        private void MouseLeftClick(object sender, EventArgs e)
+        {
+            if(isActive && !hoverForm)
+            {
+                Clipboard.SetText("#" + (c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2")));
+                isActive = false;
+                pick_btn.Text = "Pick";
+                pick_btn.Enabled = true;
+                Update_timer.Enabled = false;
+                previewF.Hide();
+                previewFshadow.Hide();
+                notifyIcon1.ShowBalloonTip(2000, "cPick", "#" + (c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2")) + " \nColor copied to clipboard.", ToolTipIcon.Info);
+                
+            }
+        }
+
+        private void NotifyIcon1_BalloonTipShown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipShown += NotifyIcon1_BalloonTipShown;
+            notifyIcon1.BalloonTipClicked += NotifyIcon1_BalloonTipClicked;
+
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            previewFshadow = new Form();
+            previewFshadow.BackColor = Color.Black;
+            previewFshadow.Opacity = 0.2;
+            previewFshadow.FormBorderStyle = FormBorderStyle.None;
+            previewFshadow.Bounds = Screen.PrimaryScreen.Bounds;
+            previewFshadow.TopMost = false;
+            previewFshadow.Size = new Size(42, 42);
+
+
+            previewF = new Form();
+            previewF.BackColor = Color.White;
+            previewF.FormBorderStyle = FormBorderStyle.None;
+            previewF.Bounds = Screen.PrimaryScreen.Bounds;
+            previewF.TopMost = true;
+            previewF.Size = new Size(40, 40);
+
+            Application.EnableVisualStyles();
+            previewFshadow.ShowInTaskbar = false;
+
+            previewF.ShowInTaskbar = false;
+        }
+
+        private void NotifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Focus();
+        }
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+        
+        private void Update_timer_Tick(object sender, EventArgs e)
+        {
+            if(isActive)
+            {
+                Point cursor = new Point();
+                GetCursorPos(ref cursor);
+
+                c = GetColorAt(cursor);
+
+                color_lbl.Text = c.R.ToString() + ", " + c.G.ToString() + ", " + c.B.ToString() + " - #" + (c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2"));
+
+                colorPanel_pnl.BackColor = c;
+
+                if (!previewF.Visible) { previewF.Show(); previewFshadow.Show(); }
+
+                previewF.BackColor = c;
+                previewF.Location = new Point(cursor.X + 15, cursor.Y + 20);
+
+
+                previewFshadow.Location = new Point(cursor.X + 17, cursor.Y + 21);
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void pick_btn_Click(object sender, EventArgs e)
+        {
+            if(isActive)
+            {
+                Update_timer.Enabled = false;
+                isActive = false;
+                pick_btn.Text = "Pick";
+                pick_btn.Enabled = true;
+                previewF.Hide(); previewFshadow.Hide();
+            }
+            else
+            {
+                Update_timer.Enabled = true;
+                isActive = true;
+                pick_btn.Enabled = false;
+                pick_btn.Text = "Picking...";
+                previewFshadow.Show();
+
+                previewF.Show();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.Focus();
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+            if(this.Visible)
+            {
+                this.ShowInTaskbar = true;
+            }
+            else
+            {
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            hoverForm = true;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            hoverForm = false;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape && isActive)
+            {
+                Update_timer.Enabled = false;
+                isActive = false;
+                pick_btn.Text = "Pick";
+                pick_btn.Enabled = true;
+                previewF.Hide(); previewFshadow.Hide();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://material.io/guidelines/style/color.html#color-color-palette");
+        }
+    }
+
+    public static class MouseHook
+    {
+        public static event EventHandler MouseAction = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+
+
+        }
+        public static void stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                  GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction(null, new EventArgs());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+    }
+}
